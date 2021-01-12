3668914b728462373ffa0e9f44c5e68b37bc22d6
fukiran <macdorphoto@yahoo.com>
2021-01-12 18:14:17 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el:"#app",
+        data: {
+            rates: []
+        },
+        mounted(){
+            this.getRates()
+        },
+        methods: {
+            getRates: function(){
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(response => response.json())
+                .then(data => this.rates = data)
+            }
+            }
+        }
+    )
+});
